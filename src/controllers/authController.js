b53850f6b06a7df8d57c1c7775204a58f825ccf6
Krissada Singhakachain <omsin_47@hotmail.com>
2024-11-19 17:44:26 +0700

    moved REDIRECT_URI to .env

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,12 +4,10 @@ export const handleOAuthRedirect = async (req, res) => {
 	if (!code) return res.status(400).send('Missing code query');
 	console.log(code);
 
-	const REDIRECT_URI = 'http://localhost:3001/auth/redirect';
-
 	const data = {
 		'grant_type': 'authorization_code',
 		'code': code,
-		'redirect_uri': REDIRECT_URI,
+		'redirect_uri': process.env.REDIRECT_URI,
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
 	};
@@ -20,7 +18,7 @@ export const handleOAuthRedirect = async (req, res) => {
 	const { access_token } = await fetch(`https://discord.com/api/oauth2/token`, { method: 'POST', headers, body: new URLSearchParams(data) }).then(r => r.json());
 
 	// if (!access_token) return res.status(400).send('probably invalid code');
-	if (!access_token) return res.redirect(process.env.FRONTEND_URL);
+	// if (!access_token) return res.redirect(process.env.FRONTEND_URL);
 
 	// res.send(`Hello ${access_token}`);
 
