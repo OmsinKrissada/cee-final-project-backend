1f39bb508d4ed57ab929d6c838c7ca4090f75259
Krissada Singhakachain <omsin_47@hotmail.com>
2024-12-03 14:02:11 +0700

    do not try to catch unhandled exception so the full trace is displayed instead of shortened one

--- src/server.js
+++ src/server.js
@@ -3,20 +3,20 @@ import "./config/db.js";
 
 import app from "./app.js";
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  console.log(err.stack);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+//   console.log(err.name, err.message);
+//   console.log(err.stack);
+//   process.exit(1);
+// });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(`${err}`);
-  // server.close(() => {
-  process.exit(1);
-  // });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+//   console.log(`${err}`);
+//   // server.close(() => {
+//   process.exit(1);
+//   // });
+// });
 
 const PORT = 3001;
 app.listen(PORT, "0.0.0.0", () => {
