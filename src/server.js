23fbb364ce7b32e3cd2738ccf7550e245a893864
Krissada Singhakachain <omsin_47@hotmail.com>
2024-12-05 04:08:48 +0700

    feat: allow setting port from environment variable

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ import app from "./app.js";
 //   // });
 // });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend Server ready at http://localhost:${PORT}`);
 });
